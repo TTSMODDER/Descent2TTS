761f8809996a79f2345341fd077c37ed05da6d67
Rouven85 <r.arheidt@gmail.com>
2024-12-31 13:36:09 +0100

    241231_Würfelscript_002

--- main/Wuerfelscript.lua
+++ main/Wuerfelscript.lua
@@ -5,61 +5,126 @@ function createWuerfelButton()
         {
             tag="VerticalLayout",
             attributes={
-                height=100,
+                height=350,
                 width=50,
                 color="rgba(0,0,0,0.7)",
                 position = "-25 0",
                 anchorMin="1 0.5",
                 anchorMax="1 0.5",
-            },
-            children={
-                {
-                    tag="Button",
-                    attributes={
-                        height=50,
-                        width = 50,
-                        color="Blue",
-                        onClick = "wuerfeln",
-                        id = "Blue",
-                    },
-                    value="Example",
+                id = "wuerfelMenu"
                 },
-                {
-                    tag="Button",
-                    attributes={
-                        height=50,
-                        width = 50,
-                        color="red",
-                        onClick = "wuerfeln",
-                        id = "Red",
+                children={
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="Blue",
+                            onClick = "wuerfeln",
+                            id = "Blue",
+                        },
+                        value="",
                     },
-                    value="Example",
-                },
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="Yellow",
+                            onClick = "wuerfeln",
+                            id = "Yellow",
+                        },
+                        value="",
+                    },
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="Green",
+                            onClick = "wuerfeln",
+                            id = "Green",
+                        },
+                        value="",
+                    },
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="Grey",
+                            onClick = "wuerfeln",
+                            id = "Grey",
+                        },
+                        value="",
+                    },
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="Black",
+                            onClick = "wuerfeln",
+                            id = "Black", 
+                        },
+                        value="",
+                    },
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="Brown",
+                            onClick = "wuerfeln",
+                            id = "Brown",
+                        },
+                        value="",
+                    },
+                    {
+                        tag="Button",
+                        attributes={
+                            height=50,
+                            width = 50,
+                            color="red",
+                            onClick = "wuerfeln",
+                            id = "Red",
+                        },
+                        value="",
+                    },
+                }
             }
         }
-    })
+    )
 end
 
 function wuerfeln (player, value, id)
-    
+   
     if  allowedPlayerColors[player.color] then
         if wuerfel[id] then
             local url = wuerfel[id].url
-            spawnObjFromCloud(url)
-            log (value)
+            spawnObjFromCloud(url, id, callback, position)
         end
     end  
 end
 
-function spawnObjFromCloud (url)
+local spawnedCubes ={}
+
+function spawnObjFromCloud (url, id, callback)
     WebRequest.get(url, function(response)
         local objectJSON = response.text
         -- Objekt mit dem geladenen JSON spawnen
         local spawnedObject = spawnObjectJSON({
             json = objectJSON,
-            position = position,
+            position = {x = xValue , y = 10, z = -10},
             callback_function = function(obj)
-                obj.setName(id)
+                obj.setName(id.." Cube")
+                table.insert(spawnedCubes, obj)
+                for i = 1, #spawnedCubes do
+                    log(position)
+                    local position = {x = xValue + (i * 3), y= 10, z=-10}
+                    spawnedCubes[i].setPosition(position)
+                    log(spawnedCubes[i])
+                end
                 if callback then
                     log("wird ausgeführt")
                     callback(obj)
@@ -67,4 +132,18 @@ function spawnObjFromCloud (url)
             end
         })
     end)
+end
+
+function callback (obj)
+    
+  
+    Wait.time(function()
+        obj.roll()
+    end,0.5, 3)
+    Wait.time(function()
+        for _, cube in ipairs (spawnedCubes) do
+            destroyObject(cube)
+        end
+        spawnedCubes = {}
+    end, 10)
 end
